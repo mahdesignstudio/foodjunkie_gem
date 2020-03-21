e7beddda604a6c3e56b36775e184654c1b11a00f
Melissa Hernandez <hdez.melissaa@gmail.com>
2020-03-21 04:17:02 +0000

    Attempt to run connection with env file

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require 'pry'
 #require_relative files that live inside the app 
 require_relative "../lib/foodjunkie_gem/api"
 require_relative "../lib/foodjunkie_gem/cli"
-require_relative "../lib/foodjunkie_gem/foodjunkie_gem/version"
+require_relative "../lib/foodjunkie_gem/version"
 
 
---
+++ lib/foodjunkie_gem/cli.rb
@@ -0,0 +1,6 @@
+class CLI 
+  #need instances/objs of the class 2 call the method 
+  def connection_test
+    puts "Hello World"
+  end 
+end 
