ae867c2c34a3b1e3aced1f8222cc37d24fd3526a
Melissa Hernandez <hdez.melissaa@gmail.com>
2020-03-21 17:30:13 +0000

    update project written structure

--- lib/foodjunkie_gem/cli.rb
+++ lib/foodjunkie_gem/cli.rb
@@ -1,6 +1,16 @@
 class FoodjunkieGem::CLI 
   #need instances/objs of the class 2 call the method 
   def call 
-    puts "I am in the CLI"
+    puts "Welcome User"
+    puts "To see FoodJunkie, enter 'let's make a meal'"
+    puts "To exit, enter 'goodbye'" #when user exits, prompt 'don't leave hungry!'
   end 
+  
+  # def area
+  #     input = gets.strip.downcase
+      
+  #     if input == "list"
+  #       puts "list"
+  #   else 
+  # end
 end 
