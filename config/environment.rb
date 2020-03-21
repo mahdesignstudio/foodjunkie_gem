0a809ec6c38bbb5ceffd5435207c3c2ae18cd73b
Melissa Hernandez <hdez.melissaa@gmail.com>
2020-03-21 03:39:02 +0000

    Setup env. folder/file

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'httparty'
+require 'pry'
+
+#require_relative files that live inside the app 
+require_relative "../lib/foodjunkie_gem/api"
+require_relative "../lib/foodjunkie_gem/cli"
+require_relative "../lib/foodjunkie_gem/foodjunkie_gem/version"
+
+
--- lib/foodjunkie_gem.rb
+++ lib/foodjunkie_gem.rb
@@ -1,4 +1,4 @@
-require "foodjunkie_gem/version"
+
 
 module FoodjunkieGem
   class Error < StandardError; end
