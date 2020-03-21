c41ba29f602ef304b3613b92aae246739989bfa3
Melissa Hernandez <hdez.melissaa@gmail.com>
2020-03-21 17:58:49 +0000

    CLI skeleton structure working

--- lib/foodjunkie_gem/cli.rb
+++ lib/foodjunkie_gem/cli.rb
@@ -2,15 +2,36 @@ class FoodjunkieGem::CLI
   #need instances/objs of the class 2 call the method 
   def call 
     puts "Welcome User"
-    puts "To see FoodJunkie, enter 'let's make a meal'"
+    puts "To see FoodJunkie, enter 'hungry'"
     puts "To exit, enter 'goodbye'" #when user exits, prompt 'don't leave hungry!'
+    menu
   end 
   
-  def area
+  def menu
       input = gets.strip.downcase
       
-      if input == "list"
-        puts "list"
+    if input == "hungry"
+       area_list
+       menu # return 'menu' don't want my user to get kicked out of application  
+    elsif input == "goodbye"
+      goodbye
     else 
+      invalid_entry
+    end 
   end
+  
+    def area_list # method for when user enters 'hungry' 
+        puts "1. Area 1" #Attributes will go here 
+        puts "2. Area 2"
+        puts "3. Area 3"
+    end 
+    
+    def goodbye 
+       puts "Don't leave hungry!"
+    end 
+  
+    def invalid_entry
+      puts "Invalid entry, please try again"
+      menu
+    end 
 end 
