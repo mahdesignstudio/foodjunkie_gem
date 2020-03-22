6d8ac2e61a6f72021b4af5f9d6360b2a870a2b35
Melissa Hernandez <hdez.melissaa@gmail.com>
2020-03-22 04:08:44 +0000

    Define fetch method

--- lib/foodjunkie_gem/api.rb
+++ lib/foodjunkie_gem/api.rb
@@ -1,9 +1,11 @@
 class API 
-    def self.get_data
-        response = RestClient.get("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+    def fetch
+        url = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list' 
+        response = RestClient.get(url)
+        binding.pry
         cuisine_array = JSON.parse(response.body)["results"]
         cuisine_array.each do |cuisine|
-          Foodjunkie.new(cuisine)
-        end 
+        #   Foodjunkie.new(cuisine)
+         end 
     end     
 end 
