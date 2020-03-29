a126918a5927e1196beb67bb01ef7dcb5165c729
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-29 18:30:46 -0400

    update cli.rb methods

--- lib/foodjunkie_gem/cli.rb
+++ lib/foodjunkie_gem/cli.rb
@@ -1,13 +1,19 @@
 class FoodjunkieGem::CLI 
 
   def call 
-    puts "Welcome to FoodJunkie!"
+    puts "*********************************************"
+    puts "*                                           *"
+    puts "*            Welcome to FoodJunkie        *"
+    puts "*                                           *"
+    puts "*********************************************"
     sleep(2)
     puts ""
     puts "To see meals based on Cuisine Types, enter 'hungry'"
     sleep(2)
+    puts ""
     puts "To exit, enter 'goodbye'" #when user exits, prompt 'don't leave hungry!'
     sleep(2)
+    puts ""
     #API.fetch
     menu
   end 
@@ -31,13 +37,15 @@ class FoodjunkieGem::CLI
         # Foodjunkie.all.each_with_index do |cuisine, index|
         #     puts "#(index + 1). #{cuisine.name}"
         # end 
-        # puts ""
-        # puts ""
+        puts ""
+        sleep(2)
+        puts "******|******|******|******|******|******|******|"
         puts "Which Cuisine type would you like to try today? "
+        puts "******|******|******|******|******|******|******|"
         sleep(2)
         puts ""
-        puts "Please select a number from the list below to find meals in this Cuisine type!"
-        sleep(2)
+        puts "[Please select a number from the list below to find meals in this Cuisine type.]"
+        sleep(1)
         puts ""
         #  input = gets.strip.downcase
         
