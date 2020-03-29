a77820e325549577da51e85460f6552126913249
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-28 22:47:42 -0400

    update api.rb file

--- lib/foodjunkie_gem/api.rb
+++ lib/foodjunkie_gem/api.rb
@@ -1,11 +1,15 @@
 class API 
-    def fetch
-        url = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list' 
-        response = RestClient.get(url)
-        binding.pry
-        cuisine_array = JSON.parse(response.body)["results"]
-        cuisine_array.each do |cuisine|
+    def self.get_cuisines
+        response = HTTParty.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+        #binding.pry
+        #cuisine_array = JSON.parse(response.body)["results"]
+        #cuisine_array.each do |cuisine|
         #   Foodjunkie.new(cuisine)
+        #response['meals'].map(&:values).flatten
+        @@all_cuisines = response['meals'].map {|hash| hash['strArea']}
+    end 
+
+         def self.all_cuisines
+            @@all_cuisines
          end 
     end     
-end 
