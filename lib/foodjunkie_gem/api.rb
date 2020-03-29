5bd21d313bda7616cb55a44f691cb9f41960034c
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-29 19:26:56 -0400

    define get_cuisine_meals method

--- lib/foodjunkie_gem/api.rb
+++ lib/foodjunkie_gem/api.rb
@@ -1,15 +1,24 @@
 class API 
+     attr_accessor :strArea    
+
+
     def self.get_cuisines
-        response = HTTParty.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+        cuisines_response = HTTParty.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
         #binding.pry
         #cuisine_array = JSON.parse(response.body)["results"]
         #cuisine_array.each do |cuisine|
         #   Foodjunkie.new(cuisine)
         #response['meals'].map(&:values).flatten
-        @@all_cuisines = response['meals'].map {|hash| hash['strArea']}
+        @@all_cuisines = cuisines_response['meals'].map {|hash| hash['strArea']}
     end 
 
-         def self.all_cuisines
-            @@all_cuisines
-         end 
-    end     
+    def self.all_cuisines
+        @@all_cuisines
+    end 
+
+    def self.get_cuisine_meals
+        meals_response = HTTParty.get('https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian')
+        
+        binding.pry
+    end
+end     
