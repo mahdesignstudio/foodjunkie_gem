bac93930def93f4cb543c4ea521aa251a2583f8b
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-29 23:11:34 -0400

    create attributes and methods

--- lib/foodjunkie_gem/foodjunkie.rb
+++ lib/foodjunkie_gem/foodjunkie.rb
@@ -1,21 +1,26 @@
 class Foodjunkie 
-    attr_accessor :name  
+    attr_accessor :strMeal, :strCategory, :strArea, :strInstructions 
     
     @@all = []
     
     def initialize(attr_hash)
-      attr_hash.each do |k, v|
-        self.send("#{k}=", v) if self.respond_to?("#{k}=")
-      end 
-        save
-    end
-    
-    def save 
-      @@all << self 
+      update(attr_hash)
+      @@all.push(self)
     end 
+    
+    
+    
+    # def save 
+    #   @@all << self 
+    # end 
     
     def self.all 
       @@all
+    end 
+
+    def update(attr_hash)
+      attr_hash.each do |k, v|
+        self.send("#{k}=", v) if self.respond_to?("#{k}=")
     end 
     
     def self.find_by_name(name)
