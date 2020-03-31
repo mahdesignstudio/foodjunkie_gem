5dc0806f63d176f56fe74cf4e323a09add0676ea
mahdesignstudio <mahdesignstudio@gmail.com>
2020-03-30 23:26:19 -0400

    update require_relative for meals.rb

--- lib/foodjunkie_gem.rb
+++ lib/foodjunkie_gem.rb
@@ -8,7 +8,8 @@ require_relative "./foodjunkie_gem/version"
  
 require_relative './foodjunkie_gem/api.rb'
 require_relative './foodjunkie_gem/cli.rb'
-require_relative './foodjunkie_gem/foodjunkie.rb'
+require_relative './foodjunkie_gem/meals.rb'
+require_relative './foodjunkie_gem/cuisines.rb'
  
 
 
